94b58ccdead18fa6eb018a628d019323d77e5134
LiYan1988 <li.yan.ly414@gmail.com>
2017-01-16 08:03:19 +0100

    Try different sizes

---
+++ cluster_6/90_10/A1_pod100/traffic_matrix_6/A1_6.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  4 15:15:10 2016
+
+@author: li
+
+optimize both throughput and connections
+"""
+
+#import sys
+#sys.path.insert(0, '/home/li/Dropbox/KTH/numerical_analysis/ILPs')
+from sdm import *
+from gurobipy import *
+import pandas as pd
+np.random.seed(2010)
+
+num_cores=7
+num_slots=80
+
+mtridx = 6 
+time_limit_routing = 1800 # 1000
+time_limit_sa = 108 # 10800
+
+filename = 'traffic_matrix_6.csv'
+#    print filename
+tm = []
+with open(filename) as f:
+    reader = csv.reader(f)
+    for idx, row in enumerate(reader):
+        row = [float(u) for u in row]
+        tm.append(row)
+
+tm = np.array(tm)
+
+betav = np.array([0, 
+    1e-5, 2e-5, 4e-5, 8e-5, 
+    1e-4, 2e-4, 4e-4, 8e-4, 
+    1e-3, 2e-3, 4e-3, 8e-3, 
+    1e-2, 2e-2, 4e-2, 8e-2, 
+    1e-1, 2e-1, 4e-1, 1, 10])
+#betav = np.array([1e-3, 2e-3, 4e-3, 8e-3])
+results = {}
+obj_results = {}
+cnk_results = {}
+thp_results = {}
+obj_ub = {}
+cnk_ub = {}
+thp_ub = {}
+
+for beta in betav:        
+    m = Arch1_decompose(tm, num_slots=num_slots, num_cores=num_cores, 
+        alpha=1,beta=beta)
+    m.create_model_routing(mipfocus=1,timelimit=time_limit_routing,mipgap=0.05, method=2)
+    m.multiple_heuristic()
+
+    results[beta] = pd.DataFrame(m.heuristics_results)
+    obj_results[beta] = results[beta].iloc[0, :]
+    cnk_results[beta] = results[beta].iloc[1, :]
+    thp_results[beta] = results[beta].iloc[2, :]
+    obj_ub[beta] = m.obj_ub_
+    cnk_ub[beta] = m.connection_ub_
+    thp_ub[beta] = m.throughput_ub_
+
+    # write results
+    m.write_result_csv('cnklist_heuristic_%d_%.2e.csv'%(mtridx,beta), m.cnklist_)
+
+
+obj_results = pd.DataFrame(obj_results)
+cnk_results = pd.DataFrame(cnk_results)
+thp_results = pd.DataFrame(thp_results)
+obj_ub = pd.Series(obj_ub)
+cnk_ub = pd.Series(cnk_ub)
+thp_ub = pd.Series(thp_ub)
+argmax = {betav[i]:obj_results.iloc[:, i].argmax() for i in range(len(betav))}
+objmax = {betav[i]:obj_results.iloc[:, i].max() for i in range(len(betav))}
+cnk_bh = {betav[i]:cnk_results.loc[argmax[betav[i]], betav[i]] 
+              for i in range(len(betav))}
+thp_bh = {betav[i]:thp_results.loc[argmax[betav[i]], betav[i]] 
+              for i in range(len(betav))}
+obj_final = pd.DataFrame({'ub':obj_ub, 'best_heuristic':objmax, 
+                          'best_method':argmax, 'cnk_bh':cnk_bh, 
+                          'thp_bh':thp_bh, 'cnk_ub':cnk_ub, 'thp_ub':thp_ub})
+obj_final['optimality'] = obj_final['best_heuristic']/obj_final['ub']
+
+obj_results.to_csv('obj_results_{}.csv'.format(mtridx))
+cnk_results.to_csv('cnk_results_{}.csv'.format(mtridx))
+thp_results.to_csv('thp_results_{}.csv'.format(mtridx))
+obj_final.to_csv('obj_final_{}.csv'.format(mtridx))
---
+++ cluster_7/prepareFilesA3.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 08 09:20:59 2017
+
+@author: celin
+
+simulations for A3
+"""
+
+import os
+from sdm import *
+from shutil import copyfile
+import pandas as pd
+
+def copy_template(src, dst, replace_lines):
+    destination = open(dst, 'wb')
+    source = open(src, 'rU')
+    for l, line in enumerate(source):
+        if l in replace_lines.keys():
+            destination.write(replace_lines[l])
+        else:
+            destination.write(line)
+    source.close()
+    destination.close()
+
+np.random.seed(2016)
+
+num_pods=100
+max_pod_connected=35
+min_pod_connected=10
+num_cores=5
+num_slots=80
+
+data_rate_choice = [50, 400]
+
+data_rate_probs = {}
+data_rate_probs['90_10'] = [0.90, 0.10]
+
+rootdir = os.getcwd()
+num_stat = []
+thp_stat = []
+for k, v in data_rate_probs.items():
+    # make simulation directory
+    if not os.path.exists(k):
+        os.mkdir(k)
+    print k
+    os.chdir(k)
+    # make traffic matrix directory
+    tm_folder = 'trafficMatrix'
+    num_tmp = []
+    thp_tmp = []
+    num_stat.append({i:0 for i in data_rate_choice})
+    thp_stat.append({i:0 for i in data_rate_choice})
+    if not os.path.exists(tm_folder):
+        os.mkdir(tm_folder)
+    os.chdir(tm_folder)
+    for i in range(20):
+        t = Traffic(num_pods=num_pods, max_pod_connected=max_pod_connected, 
+                    min_pod_connected=min_pod_connected, 
+                    capacity_choices=data_rate_choice,
+                    capacity_probs=v)
+        t.generate_traffic()
+        filename='traffic_matrix_{}.csv'.format(i)
+        df = pd.DataFrame(t.traffic_matrix)
+        df.to_csv(filename, index=False, header=False)
+        t.value_count()
+        num_tmp.append(t.number_count)
+        thp_tmp.append(t.throughput_count)
+        for j in data_rate_choice:
+            num_stat[-1][j] += num_tmp[-1][j]
+            thp_stat[-1][j] += thp_tmp[-1][j]
+    num_sum = sum(num_stat[-1].values())
+    thp_sum = sum(thp_stat[-1].values())
+    num_stat[-1] = {i:1.*num_stat[-1][i]/num_sum for i in data_rate_choice}
+    thp_stat[-1] = {i:1.*thp_stat[-1][i]/thp_sum for i in data_rate_choice}
+    os.chdir('../')
+    
+    # make A1 directory
+    workdir = 'A1_pod100'
+    if not os.path.exists(workdir):
+        os.mkdir(workdir)
+    os.chdir(workdir)
+    for i in range(20):
+        # write .py file
+        src = os.path.join(rootdir, 'templateA1.py')
+        tm_name = 'traffic_matrix_{}'.format(i)
+        if not os.path.exists('traffic_matrix_{}'.format(i)):
+            os.mkdir(tm_name)
+        os.chdir(tm_name)
+        line16 = "num_cores={}\n".format(num_cores)
+        line17 = "num_slots={}\n".format(num_slots)
+        line19 = "mtridx = {} \n".format(i)
+        line23 = "filename = 'traffic_matrix_{}.csv'\n".format(i)
+        replace_lines = {16:line16, 17:line17, 19:line19, 23:line23}
+        dst = 'A1_{}.py'.format(i)
+        copy_template(src, dst, replace_lines)
+#        copyfile('../../../sdm.py', 'sdm.py')
+#        copyfile('../../{}/traffic_matrix_{}.csv'.format(tm_folder, i), 
+#                 './traffic_matrix_{}.csv'.format(i))
+        # write .sh file
+        src = os.path.join(rootdir, 'templateA1.sh')
+        replace_lines = {}
+        replace_lines[2] = "#SBATCH -p hebbe\n" # change if use hebbe
+        replace_lines[3] = "#SBATCH -J A1_{}-{}\n".format(i, k)
+        replace_lines[7] = "#SBATCH -o A1_{}-{}.stdout\n".format(i, k)
+        replace_lines[8] = "#SBATCH -e A1_{}-{}.stderr\n".format(i, k)
+        replace_lines[16] = "pdcp ../../trafficMatrix/traffic_matrix_"+str(i)+\
+            ".csv $TMPDIR\n"
+        replace_lines[17] = "pdcp ../../../sdm.py $TMPDIR"
+        replace_lines[21] = "python A1_{}.py\n".format(i)
+        dst = 'A1_{}.sh'.format(i)
+        copy_template(src, dst, replace_lines)
+        os.chdir('../')
+        
+    # make A2 directory
+    os.chdir('../')
+    workdir = 'A2_pod100'
+    if not os.path.exists(workdir):
+        os.mkdir(workdir)
+    os.chdir(workdir)
+    for i in range(20):
+        # write .py file
+        src = os.path.join(rootdir, 'templateA2.py')
+        tm_name = 'traffic_matrix_{}'.format(i)
+        if not os.path.exists('traffic_matrix_{}'.format(i)):
+            os.mkdir(tm_name)
+        os.chdir(tm_name)
+        line16 = "num_cores={}\n".format(num_cores)
+        line17 = "num_slots={}\n".format(num_slots)
+        line19 = "mtridx = {} \n".format(i)
+        line23 = "filename = 'traffic_matrix_{}.csv'\n".format(i)
+        replace_lines = {16:line16, 17:line17, 19:line19, 23:line23}
+        dst = 'A2_{}.py'.format(i)
+        copy_template(src, dst, replace_lines)
+#        copyfile('../../../sdm.py', 'sdm.py')
+#        copyfile('../../{}/traffic_matrix_{}.csv'.format(tm_folder, i), 
+#                 './traffic_matrix_{}.csv'.format(i))
+        # write .sh file
+        src = os.path.join(rootdir, 'templateA2.sh')
+        replace_lines = {}
+        replace_lines[2] = "#SBATCH -p hebbe\n" # change if use hebbe
+        replace_lines[3] = "#SBATCH -J A2_{}-{}\n".format(i, k)
+        replace_lines[7] = "#SBATCH -o A2_{}-{}.stdout\n".format(i, k)
+        replace_lines[8] = "#SBATCH -e A2_{}-{}.stderr\n".format(i, k)
+        replace_lines[16] = "pdcp ../../trafficMatrix/traffic_matrix_"+str(i)+\
+            ".csv $TMPDIR\n"
+        replace_lines[17] = "pdcp ../../../sdm.py $TMPDIR"
+        replace_lines[21] = "python A2_{}.py\n".format(i)
+        dst = 'A2_{}.sh'.format(i)
+        copy_template(src, dst, replace_lines)
+        os.chdir('../')
+        
+    # make A3 directory
+    os.chdir('../')
+    workdir = 'A3_pod100'
+    if not os.path.exists(workdir):
+        os.mkdir(workdir)
+    os.chdir(workdir)
+    for i in range(20):
+        # write .py file
+        src = os.path.join(rootdir, 'templateA3.py')
+        tm_name = 'traffic_matrix_{}'.format(i)
+        if not os.path.exists('traffic_matrix_{}'.format(i)):
+            os.mkdir(tm_name)
+        os.chdir(tm_name)
+        line16 = "num_cores={}\n".format(num_cores)
+        line17 = "num_slots={}\n".format(num_slots)
+        line19 = "mtridx = {} \n".format(i)
+        line23 = "filename = 'traffic_matrix_{}.csv'\n".format(i)
+        replace_lines = {16:line16, 17:line17, 19:line19, 23:line23}
+        dst = 'A3_{}.py'.format(i)
+        copy_template(src, dst, replace_lines)
+#        copyfile('../../../sdm.py', 'sdm.py')
+#        copyfile('../../{}/traffic_matrix_{}.csv'.format(tm_folder, i), 
+#                 './traffic_matrix_{}.csv'.format(i))
+        # write .sh file
+        src = os.path.join(rootdir, 'templateA3.sh')
+        replace_lines = {}
+        replace_lines[2] = "#SBATCH -p hebbe\n" # change if use hebbe
+        replace_lines[3] = "#SBATCH -J A3_{}-{}\n".format(i, k)
+        replace_lines[7] = "#SBATCH -o A3_{}-{}.stdout\n".format(i, k)
+        replace_lines[8] = "#SBATCH -e A3_{}-{}.stderr\n".format(i, k)
+        replace_lines[16] = "pdcp ../../trafficMatrix/traffic_matrix_"+str(i)+\
+            ".csv $TMPDIR\n"
+        replace_lines[17] = "pdcp ../../../sdm.py $TMPDIR"
+        replace_lines[21] = "python A3_{}.py\n".format(i)
+        dst = 'A3_{}.sh'.format(i)
+        copy_template(src, dst, replace_lines)
+        os.chdir('../')
+    os.chdir('../../')
