c72b27aa16ac778df615a78139293d3c9ef031ba
Li Yan <li.yan.ly414@gmail.com>
2017-12-01 15:29:35 +0100

    Add more simulations

---
+++ revision/revision_simulations/A1_A3_mice_50_25_elep_400_25_L1_0.1_L2_0.95/templateA1.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#SBATCH -A C3SE407-15-3
+#SBATCH -p hebbe
+#SBATCH -J arch2_old_1
+#SBATCH -N 1
+#SBATCH -n 4
+#SBATCH -t 12:00:00
+#SBATCH -o arch2_old_1.stdout
+#SBATCH -e arch2_old_1.stderr
+module purge 
+source ~/.usr_path_grb_py27
+
+
+
+
+pdcp *.py $TMPDIR
+pdcp traffic_matrix_* $TMPDIR
+pdcp ../../../sdm.py $TMPDIR
+
+cd $TMPDIR
+
+python A1_1.py
+
+cp *.csv $SLURM_SUBMIT_DIR
+
+# End script
---
+++ revision/revision_simulations/A1_A3_mice_50_25_elep_400_25_L1_0.1_L2_0.95/templateA3.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#SBATCH -A C3SE407-15-3
+#SBATCH -p hebbe
+#SBATCH -J arch5_old_0
+#SBATCH -N 1
+#SBATCH -n 4
+#SBATCH -t 12:00:00
+#SBATCH -o arch5_old_0.stdout
+#SBATCH -e arch5_old_0.stderr
+module purge 
+source ~/.usr_path_grb_py27
+
+
+
+
+pdcp *.py $TMPDIR
+pdcp traffic_matrix_* $TMPDIR
+pdcp ../../../sdm.py $TMPDIR
+
+cd $TMPDIR
+
+python pareto0.py
+
+cp *.csv $SLURM_SUBMIT_DIR
+
+# End script
