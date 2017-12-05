23975df83e19fad70e51704261b4b63dc54b8e4d
LiYan1988 <li.yan.ly414@gmail.com>
2017-12-05 14:48:25 +0100

    Plot figures for simulation 3

---
+++ revision/revision_simulations_results_3/A2_mice_30_25_elep_380_25_L1_0.05_L2_0.35/processClasses.m
@@ -0,0 +1,50 @@
+clc;
+close all;
+clear;
+
+trafficTypes = {'90_10'};
+architectureTypes = {'A2_pod100'};
+
+rootdir = pwd;
+curlist = {};
+trafficMatrices = {};
+flowAverage = zeros(3, 1);
+for i = 1:1
+    for j = 1:1
+        tmp = {rootdir, trafficTypes(i), architectureTypes(j)};
+        curlist = [curlist, joinPath(tmp)];
+    end
+    trafficMatrixPath = joinPath({rootdir, trafficTypes(i), {'trafficMatrix'}});
+    flowAverage(i) = trafficAverage(trafficMatrixPath);
+end
+
+%%
+[mouseConnection, mouseThroughput, elephantConnection, elephantThrought, beta] = extractClassData(curlist{1}, flowAverage(1), 20);
+
+%%
+figure;
+hold on;
+plot(mouseConnection, mouseThroughput);
+plot(elephantConnection, elephantThrought);
+
+%% 
+objConnection = mouseConnection+elephantConnection;
+objThroughput = mouseThroughput+elephantThrought;
+figure;
+hold on;
+plot(objConnection, objThroughput);
+
+%% 
+figure; 
+semilogx(beta, mouseConnection)
+hold on;
+semilogx(beta, elephantConnection);
+
+figure;
+semilogx(beta, mouseThroughput);
+hold on;
+semilogx(beta, elephantThrought);
+
+%% 
+csvName = 'A2_mice_30_25_elep_380_25_L1_0.05_L2_0.35.csv';
+csvwrite(csvName, [beta', mouseConnection, mouseThroughput, elephantConnection, elephantThrought])
--- revision/revision_simulations_results_3/plot-figures2/A2_compare.m
+++ revision/revision_simulations_results_3/plot-figures2/A2_compare.m
@@ -4,11 +4,11 @@ close all;
 
 %% A2
 % columns are: beta, mouseConnection, mouseThroughput, elephantConnection, elephantThrought
-x1 = csvread('A2_mice_25_10_elep_375_10_L1_0.05_L2_0.35.csv');
-x2 = csvread('A2_mice_50_0_elep_400_0_L1_0.05_L2_0.35-ok.csv');
-x3 = csvread('A2_mice_50_10_elep_400_20_L1_0.05_L2_0.35-good.csv');
-x4 = csvread('A2_mice_50_25_elep_400_50_L1_0.05_L2_0.35-good.csv');
-x5 = csvread('A2_mice_75_10_elep_350_20_L1_0.05_L2_0.35-bad.csv');
+x1 = csvread('A2_mice_30_25_elep_380_25_L1_0.05_L2_0.35.csv');
+x2 = csvread('A2_mice_50_0_elep_400_0_L1_0.05_L2_0.35.csv');
+x3 = csvread('A2_mice_50_25_elep_400_25_L1_0.05_L2_0.35.csv');
+x4 = csvread('A2_mice_50_50_elep_400_50_L1_0.05_L2_0.35.csv');
+x5 = csvread('A2_mice_70_25_elep_420_25_L1_0.05_L2_0.35.csv');
 beta1 = x1(:, 1);
 beta1(1) = 1e-8;
 beta2 = x2(:, 1);
@@ -26,26 +26,27 @@ x4(1, 1) = 1e-8;
 x5(1, 1) = 1e-8;
 
 %% 
-% x1 = x1([1, 8, 9, 11, 13, 14, 16], :);
+% x1 = x1([1, 8, 9, 11, 13, 16], :);
 % x1 = interp1(x1(:, 1), x1(:, 2:5), beta1, 'linear');
 % x1 = [beta1, x1];
-% x2 = x2([1, 8, 9, 11, 13, 17, 19, 21], :);
+% x2 = x2([1, 8, 9, 13, 18, 19, 20], :);
 % x2 = interp1(x2(:, 1), x2(:, 2:5), beta2, 'linear');
 % x2 = [beta2, x2];
 % x3 = x3([1, 8, 9, 11, 13, 14], :);
 % x3(:, [2, 4]) = x3(:, [2, 4]) - linspace(0, 0, 6)';
-% x3(:, [3, 5]) = x3(:, [3, 5]) - linspace(0e4, 0, 6)';
+% % x3(:, [5]) = x3(:, [5]) + linspace(1e4, 0, 6)';
+% % x3(:, [3]) = x3(:, [3]) + linspace(-5e3, 5e3, 6)';
 % x3 = interp1(x3(:, 1), x3(:, 2:5), beta3, 'linear');
 % x3 = [beta3, x3];
 % for i=15:22
 %     x3(i, 2:5) = x3(14, 2:5);
 % end
-% x4 = x4([1, 8, 9, 11, 13, 17, 19, 21], :);
+% x4 = x4([1, 8, 9, 11, 13, 17, 19], :);
 % x4 = interp1(x4(:, 1), x4(:, 2:5), beta4, 'linear');
 % x4 = [beta4, x4];
-% x5 = x5([1, 2, 7, 8, 9, 11], :);
-% x5(:, [2, 4]) = x5(:, [2, 4]) - linspace(10, 90, 6)';
-% x5(:, [3, 5]) = x5(:, [3, 5]) - linspace(1e4, 0, 6)';
+% x5 = x5([1, 2, 9, 12, 13, 14, 18], :);
+% % x5(:, [2, 4]) = x5(:, [2, 4]) - linspace(10, 90, 6)';
+% % x5(:, [3, 5]) = x5(:, [3, 5]) - linspace(1e4, 0, 6)';
 % x5 = interp1(x5(:, 1), x5(:, 2:5), beta5, 'linear');
 % x5 = [beta5, x5];
 
@@ -66,18 +67,18 @@ x5Throughput = x5(:, 3) + x5(:, 5);
 figure;
 hold on
 plot(x2Connection, x2Throughput, 'marker', '^', 'displayname', '\mu (50, 400), \sigma (0, 0)')
-plot(x3Connection, x3Throughput, 'marker', '^', 'displayname', '\mu (50, 400), \sigma (10, 10)')
-plot(x4Connection, x4Throughput, 'marker', '^', 'displayname', '\mu (50, 400), \sigma (25, 25)')
+plot(x3Connection, x3Throughput, 'marker', '^', 'displayname', '\mu (50, 400), \sigma (25, 25)')
+plot(x4Connection, x4Throughput, 'marker', '^', 'displayname', '\mu (50, 400), \sigma (50, 50)')
 legend('show')
-csvwrite('A2-pareto-fix-mean.csv', [x2Connection, x2Throughput, x3Connection, x3Throughput, x4Connection, x4Throughput])
+% csvwrite('A2-pareto-fix-mean.csv', [x2Connection, x2Throughput, x3Connection, x3Throughput, x4Connection, x4Throughput])
 %% Plot Pareto curve, different mean (25, 450), (50, 400), (75, 350), same std 10
 figure;
 hold on
-plot(x1Connection, x1Throughput, 'marker', '^', 'displayname', '\mu (25, 350), \sigma (10, 10)')
+plot(x1Connection, x1Throughput, 'marker', '^', 'displayname', '\mu (30, 380), \sigma (10, 10)')
 plot(x3Connection, x3Throughput, 'marker', '^', 'displayname', '\mu (50, 400), \sigma (10, 10)')
-plot(x5Connection, x5Throughput, 'marker', '^', 'displayname', '\mu (75, 450), \sigma (10, 10)')
+plot(x5Connection, x5Throughput, 'marker', '^', 'displayname', '\mu (80, 420), \sigma (10, 10)')
 legend('show')
-csvwrite('A2-pareto-fix-std.csv', [x1Connection, x1Throughput, x3Connection, x3Throughput, x5Connection, x5Throughput])
+% csvwrite('A2-pareto-fix-std.csv', [x1Connection, x1Throughput, x3Connection, x3Throughput, x5Connection, x5Throughput])
 %% Plot blocking of both classes
 figure;
 % semilogx(beta1, x1(:, 2)/1800, 'displayname', 'mouse~(25, 10)')
@@ -88,9 +89,9 @@ figure;
 % hold on;
 % semilogx(beta2, x2(:, 4)/200, 'displayname', 'elephant~(400, 0)')
 % hold on;
-semilogx(beta3, x3(:, 2)/1800, 'displayname', 'mouse~(50, 10)')
+semilogx(beta3, x3(:, 2)/2200, 'displayname', 'mouse~(50, 25)')
 hold on;
-semilogx(beta3, x3(:, 4)/200, 'displayname', 'elephant~(400, 10)')
+semilogx(beta3, x3(:, 4)/240, 'displayname', 'elephant~(400, 25)')
 % hold on;
 % semilogx(beta4, x4(:, 2)/1800, 'displayname', 'mouse~(50, 25)')
 % hold on;
@@ -100,20 +101,20 @@ semilogx(beta3, x3(:, 4)/200, 'displayname', 'elephant~(400, 10)')
 % hold on;
 % semilogx(beta5, x5(:, 4)/200, 'displayname', 'elephant~(450, 10)')
 legend('show')
-csvwrite('A2-blocking-both-classes.csv', [beta3, x3(:, 2)/1800, x3(:, 4)/200])
+% csvwrite('A2-blocking-both-classes.csv', [beta3, x3(:, 2)/2200, x3(:, 4)/240])
 %% Plot throughput of both classes
 figure;
-% semilogx(beta1, x1(:, 3), 'displayname', 'mouse~(25, 10)')
+% semilogx(beta1, x1(:, 3)/1e3, 'displayname', 'mouse~(25, 10)')
 % hold on;
-% semilogx(beta1, x1(:, 5), 'displayname', 'elephant~(350, 10)')
+% semilogx(beta1, x1(:, 5)/1e3, 'displayname', 'elephant~(350, 10)')
 % hold on;
-% semilogx(beta2, x2(:, 3), 'displayname', 'mouse~(50, 0)')
+% semilogx(beta2, x2(:, 3)/1e3, 'displayname', 'mouse~(50, 0)')
 % hold on;
-% semilogx(beta2, x2(:, 5), 'displayname', 'elephant~(400, 0)')
+% semilogx(beta2, x2(:, 5)/1e3, 'displayname', 'elephant~(400, 0)')
 % hold on;
-semilogx(beta3, x3(:, 3)/1e3, 'displayname', 'mouse~(50, 10)')
+semilogx(beta3, x3(:, 3)/1e3, 'displayname', 'mouse~(50, 25)')
 hold on;
-semilogx(beta3, x3(:, 5)/1e3, 'displayname', 'elephant~(400, 10)')
+semilogx(beta3, x3(:, 5)/1e3, 'displayname', 'elephant~(400, 25)')
 % hold on;
 % semilogx(beta4, x4(:, 3), 'displayname', 'mouse~(50, 25)')
 % hold on;
@@ -123,4 +124,4 @@ semilogx(beta3, x3(:, 5)/1e3, 'displayname', 'elephant~(400, 10)')
 % hold on;
 % semilogx(beta5, x5(:, 5), 'displayname', 'elephant~(450, 10)')
 legend('show')
-csvwrite('A2-throughput-both-classes.csv', [beta3, x3(:, 3)/1e3, x3(:, 5)/1e3])
+% csvwrite('A2-throughput-both-classes.csv', [beta3, x3(:, 3)/1e3, x3(:, 5)/1e3])
